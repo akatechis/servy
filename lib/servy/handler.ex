fec31c8cd1140a9ad6383c8dac3dab2bfc62b042
Alexandros Katechis <akatechis@gmail.com>
2025-06-30 21:52:34 -0400

    Refactor Servy Http module

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -10,7 +10,7 @@ defmodule Servy.Handler do
     |> Servy.Plugins.log
     |> route
     |> Servy.Plugins.track
-    |> format_response
+    |> Servy.Http.format_response
   end
 
   defp parse(request) do
@@ -73,22 +73,4 @@ defmodule Servy.Handler do
     Logger.error("Error reading file: #{reason}")
     %{conv | resp_body: "File not found", status_code: 500}
   end
-
-  defp format_response(conv) do
-    """
-    HTTP/1.1 #{conv.status_code} #{reason_phrase(conv.status_code)}
-    Context-Type: text/html
-    Content-Length: #{byte_size(conv.resp_body)}
-
-    #{conv.resp_body}
-    """
-  end
-
-  defp reason_phrase(200), do: "OK"
-  defp reason_phrase(201), do: "Created"
-  defp reason_phrase(401), do: "Unauthorized"
-  defp reason_phrase(403), do: "Forbidden"
-  defp reason_phrase(404), do: "Not Found"
-  defp reason_phrase(500), do: "Internal Server Error"
-  defp reason_phrase(_SC), do: "Unknown Status"
 end
